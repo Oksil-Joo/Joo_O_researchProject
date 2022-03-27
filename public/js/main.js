b6249f0e72dfd7042e26ac76a512de2f4bbbc81c
Oksil-Joo <bbkke07@gmail.com>
2022-03-26 22:35:11 -0400

    updateMediafile

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,52 @@
-import { getData } from "./component/TheDataMiner.js";
-import TheThumbnailComponent from "./component/TheThumbNail.js";
-import TheLightboxComponent from "./component/TheLightboxComponent.js";
 
 (() => {
+    const   theFavor = document.querySelector("#favour-things"),
+            theTemplate = document.querySelector("#fav-template").content;
+  
+    // let buttons = document.querySelectorAll("button");
+          
 
-    const myVue = new Vue({
-        created: function() {
-            
-            getData(null, (data) => this.researchData = data);
-        },
+    function getData() {
+        fetch("./data.json")
+        .then(res => res.json()) //unpach the API
+        .then(data => {
+            console.table(data);
+        
+            buildFavor(data);
+        })
+        .catch(error => console.error(error));
+    }
 
-        data: {
-            researchData: [],
-            message: "hello from Vue",
-            isVisible: false,
-            currentResearchItem: {}
-        },
+//     function showData() {
+        
+//         let key = this.dataset.key;
 
-        methods: {
-            popLightBox(item) {
-                this.currentResearchItem = item;
-                this.isVisible = true;
-            },
+//         let headline = document.querySelector("h2");
 
-            closeLightBox() {
-                this.isVisible = false;
-            }
-        },
+//         headline.textContent = data[key].name;
+// }
 
-        components: {
-            thumb: TheThumbnailComponent,
-            lightbox: TheLightboxComponent
-        }
-    }).$mount("#app"); 
+
+
+    function buildFavor(info) {
+       let favor = Object.keys(info);
+
+       favor.forEach(hobby => {
+           let panel = theTemplate.cloneNode(true),
+                thingInfo = panel.firstElementChild.children;
+
+                thingInfo[0].querySelector('img').src = `images/${info[hobby].pic}`;
+                thingInfo[1].textContent = info[hobby].name;
+                thingInfo[2].textContent = info[hobby].mediaType;
+                thingInfo[3].textContent = info[hobby].des;
+
+            theFavor.appendChild(panel);
+       })
+    }
+
+    getData();
+
+    // buttons.forEach(button => button.addEventListener("click", showData));
     
+
 })();
